e77c7ac0a8158d4c6d0740873e322b2e094ea0b2
maruthgoyal <maruthgoyal@gmail.com>
2017-08-09 17:00:04 +0530

    Added basics to C++ files

---
+++ src/cpp/parse.hpp
@@ -0,0 +1,10 @@
+#ifndef PARSE_INCLUDE
+#define PARSE_INCLUDE
+
+/* The parse function accepts a string containing Brain****++ code
+ * It must split the code into tokens as defined by the language specification
+ * Look at the document for an example
+ * */
+char **parse(const char *code);
+
+#endif
