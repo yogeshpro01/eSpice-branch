f1fd5cd3a5ba55baf49b2f15b21d8abaef8b7980
Yogesh Aggarwal <yogeshpro01@gmail.com>
2017-08-14 23:48:28 +0530

    Update parse.hpp

--- src/cpp/parse.hpp
+++ src/cpp/parse.hpp
@@ -5,6 +5,18 @@
  * It must split the code into tokens as defined by the language specification
  * Look at the document for an example
  * */
-char **parse(const char *code);
+
+ #include <stddef.h>
+
+struct node {
+    char c;
+    node *l , *r;
+    node() {
+        l = NULL; r = NULL;
+    }
+ };
+
+bool validate(char *code);
+node **parse(char *code);
 
 #endif
