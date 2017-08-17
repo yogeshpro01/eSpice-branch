93e7e241dbd914a1b437c05eea157a24e08488e8
Yogesh Aggarwal <yogeshpro01@gmail.com>
2017-08-17 20:16:04 +0530

    Update eval_tests.cpp

--- tests/cpp/eval_tests.cpp
+++ tests/cpp/eval_tests.cpp
@@ -9,29 +9,15 @@
 
 // Test case 1 -> INVALID COMMAND
 
-
-std::string test1 = "-.>,reg,][";
-
-
 // Test case 2 -> Adds the values 2 and 5 and prints 7
-
-std::string test2 = "++>+++++[<+>-]++++++++[<++++++>-]<.";
-
 
 // Test case 3 -> Prints "Hello World"
 
 
-std::string test3 = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
-
-
-// Test case 4 -> Accepts two characters and prints them
-
-
-std::string test4 = ",>,<.>.";
-
 void tests() {
     int pass = 0, fail = 0;
 
+    std::string test1 = "-.>,reg,][";
     node *t1 = parse(&test1[0] , test1.length());
     if ( t1 == NULL ) {
         printf("Case 1: OK \n");
@@ -40,6 +26,8 @@ void tests() {
         printf("Case 1: FAIL \n");
         fail++;
     }
+
+    std::string test2 = "++>+++++[<+>-]++++++++[<++++++>-]<.";
     node *t2 = parse(&test2[0] , test2.length());
     evaluate(t2);
     if ( stx == "7" ) {
@@ -50,6 +38,7 @@ void tests() {
         fail++;
     }
 
+    std::string test3 = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
     node *t3 = parse(&test3[0] , test3.length());
     evaluate(t3);
     if ( stx == "Hello World!\n" ) {
@@ -57,16 +46,6 @@ void tests() {
         pass++;
     } else {
         printf("Case 3: FAIL \n");
-        fail++;
-    }
-
-    node *t4 = parse(&test4[0] , test4.length());
-    evaluate(t4);
-    if ( stin == stout ) {
-        printf(" \nCase 4: OK \n");
-        pass++;
-    } else {
-        printf("Case 4: FAIL \n");
         fail++;
     }
 
