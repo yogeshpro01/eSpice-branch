47f916a16099d81dd91cd9289f863f61a6d42e00
Yogesh Aggarwal <yogeshpro01@gmail.com>
2017-08-20 23:16:58 +0530

    Update eval.hpp

--- src/cpp/eval.hpp
+++ src/cpp/eval.hpp
@@ -12,6 +12,7 @@ int command(node *cur, char c, int ptr, int mxptr, int cur_size);
 void evaluate(node *cmd);
 int pow(int base, int exp);
 int fact(int n);
+int sqrt(int n);
 void com_sp(node *cur , int ptr , int mxptr , int cur_size);
 
 #endif
