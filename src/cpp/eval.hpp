5e061d3efdf0232da364830cdefeed320748feee
Yogesh Aggarwal <yogeshpro01@gmail.com>
2017-08-20 18:03:00 +0530

    Update eval.hpp

--- src/cpp/eval.hpp
+++ src/cpp/eval.hpp
@@ -1,10 +1,22 @@
 #ifndef EVAL_INCLUDE
 #define EVAL_INCLUDE
 
-#include <iostream>
+#include "parse.hpp"
+#include "parse.cpp"
+
 #include<stdio.h>
 #include<vector>
 #include<algorithm>
 #include<string>
+
+
+int command(node * , char , int, int, int);
+void loop(node *st, int ptr, int mxptr, int cur_size);
+void gr_table(int mxptr , int cur_size);
+int command(node *cur, char c, int ptr, int mxptr, int cur_size);
+void evaluate(node *cmd);
+int pow(int base, int exp);
+int fact(int n);
+void com_sp(node *cur , int ptr , int mxptr , int cur_size);
 
 #endif
